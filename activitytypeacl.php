e2b6da1915dafd2b082c4f1f1b90fc7885b00287
Matthew Wire <mjw@mjwconsult.co.uk>
2022-06-13 22:02:56 +0100

    Regenerate civix

--- activitytypeacl.php
+++ activitytypeacl.php
@@ -31,17 +31,6 @@ function activitytypeacl_civicrm_config(&$config) {
 }
 
 /**
- * Implementation of hook_civicrm_xmlMenu
- *
- * @param $files array(string)
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function activitytypeacl_civicrm_xmlMenu(&$files) {
-  _activitytypeacl_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implementation of hook_civicrm_install
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -90,40 +79,6 @@ function activitytypeacl_civicrm_disable() {
  */
 function activitytypeacl_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _activitytypeacl_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implementation of hook_civicrm_managed
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function activitytypeacl_civicrm_managed(&$entities) {
-  _activitytypeacl_civix_civicrm_managed($entities);
-}
-
-/**
- * Implementation of hook_civicrm_caseTypes
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function activitytypeacl_civicrm_caseTypes(&$caseTypes) {
-  _activitytypeacl_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implementation of hook_civicrm_alterSettingsFolders
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function activitytypeacl_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _activitytypeacl_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -440,4 +395,22 @@ function activitytypeacl_civicrm_selectWhereClause($entity, &$clauses) {
       }
     }
   }
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function activitytypeacl_civicrm_postInstall() {
+  _activitytypeacl_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function activitytypeacl_civicrm_entityTypes(&$entityTypes) {
+  _activitytypeacl_civix_civicrm_entityTypes($entityTypes);
 }
